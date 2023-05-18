786a58848d70576c26f7e40fd456dd8501107b40
신세영 <ssy07312@naver.com>
2023-05-18 14:33:32 +0900

    actor

--- final-pjt-back/api/urls.py
+++ final-pjt-back/api/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_top_rated, name='get_top_rated'),
+    # 기본 데이터 추출
+    path('get_top_rated/', views.get_top_rated, name='get_top_rated'),
     path('genres/', views.get_genres, name='get_genres'),
+
+    # 이동할 url
+    path('', views.movie_list, name='movie_list'),
+    path('<int:movie_id>/', views.movie_detail, name='movie_detail'),
 ]
